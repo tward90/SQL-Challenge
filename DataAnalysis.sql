74248e8030b62e7507f037d068c4eaf0fdaad03c
Col D <60669354+tward90@users.noreply.github.com>
2020-07-10 09:48:10 -0500

    Completed Part 2 through question 6.

--- DataAnalysis.sql
+++ DataAnalysis.sql
@@ -9,15 +9,41 @@ ON (e.emp_no = s.emp_no);
 
 SELECT first_name, last_name, hire_date 
 FROM employees
-WHERE hire_date between '1986-01-01' and '1986-12-31'
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM employees e
+   JOIN dept_manager dm
+   		ON dm.emp_no = e.emp_no
+   JOIN departments d
+   		ON d.dept_no = dm.dept_no
+
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+   JOIN dept_emp
+   		ON dept_emp.emp_no = e.emp_no
+   JOIN departments d
+   		ON d.dept_no = dept_emp.dept_no
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
+SELECT first_name, last_name, sex
+FROM employees e
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+   JOIN dept_emp
+   		ON dept_emp.emp_no = e.emp_no
+   JOIN departments d
+   		ON d.dept_no = dept_emp.dept_no
+WHERE d.dept_name = 'Sales'
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
